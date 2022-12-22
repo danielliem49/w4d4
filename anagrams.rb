e63742898a9db12c5a583304f717ad67efd0bc48
Daniel Liem <danielliem49@gmail.com>
2022-12-22 14:59:12 -0800

    Add anagrams

---
+++ anagrams.rb
@@ -0,0 +1,98 @@
+def first_anagram?(str1, str2)
+    str1_ana_chars = str1.chars.permutation(str2.length).to_a
+    str1_anagrams = str1_ana_chars.map {|word| word.join}
+    str1_anagrams.include?(str2)
+end
+# O(n!)
+
+# first_anagram?("gizmo", "sally")    #=> false
+# first_anagram?("elvis", "lives")    #=> true
+
+def second_anagram?(str1, str2) #elvis , lives
+    str2_arr = str2.split("") 
+    str1.each_char.with_index do |ele, i|
+        j = str2_arr.index(str1[i])
+        if j == nil
+            return false
+        end
+        str2_arr.delete_at(j)
+    end
+    str2_arr.length == 0
+end
+# O(n^2)
+
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+
+def third_anagram?(str1, str2)
+
+    str1_indices = string_to_index(str1)
+    str2_indices = string_to_index(str2)
+
+    str1_sorted = merge_sort(str1_indices)
+    str2_sorted = merge_sort(str2_indices)
+
+    if str1_sorted == str2_sorted
+        return true
+    end
+    false
+end
+#  O(nlogn)
+
+                        def string_to_index(str)
+                            result = []
+                            alpha = ("a".."z").to_a
+                            str.each_char do |ele|
+                                result << alpha.index(ele)
+                            end
+                            result
+                        end
+
+                        def merge_sort(arr, &prc)
+                        prc ||= Proc.new {|a,b| a <=> b}
+
+                        mid = arr.length / 2
+                        return arr if arr.length < 2
+                        left = arr.take(mid)
+                        right = arr.drop(mid)
+
+                        left_sorted = merge_sort(left, &prc)
+                        right_sorted = merge_sort(right, &prc)
+
+                            merge(left_sorted, right_sorted, &prc)
+                        end
+
+                        def merge(left, right, &prc)
+                            merged = []
+                                until left.empty? || right.empty?
+                                    if prc.call(left[0], right[0]) == -1
+                                        merged << left.shift
+                                    else
+                                        merged << right.shift
+                                    end
+                                end
+                            merged + left + right
+                        end
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+# p third_anagram?("julko", "okujl")
+
+def fourth_anagram?(str1, str2)
+    count1 = Hash.new(0)
+    count2 = Hash.new(0)
+
+        str1.each_char do |char|
+            count1[char] += 1
+        end
+
+        str2.each_char do |char|
+            count2[char] += 1
+        end
+
+    count1 == count2
+end
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
--- execution_time_diff.rb
+++ execution_time_diff.rb
@@ -25,41 +25,54 @@ end
 # list = [ -1, 3, 5, 4, -5, 10, 1, 90 ]
 #  p my_min_phase1(list)  # =>  -5
 
- def lcs_phase1(arr)
-      sums = []
-        arr.each_with_index do |ele1, i|
-            ((i + 1)...arr.length).each do |j|
-                sums << arr[i..j]
-            end
-        end
-        unsorted = sums.map {|subarr| subarr.sum}
-        unsorted.sort[-1]
- end
- 
 #  def lcs_phase1(arr)
-#     max_sum = 0
-
-#     # debugger
-#     arr.each_with_index do |ele1, i|
-#         cumulative_sum = ele1
-#         arr[(i + 1)..-1].each do |ele2|
-#             cumulative_sum += ele2
-#             if max_sum < cumulative_sum
-#                 max_sum = cumulative_sum
+#       sums = []
+#         arr.each_with_index do |ele1, i|
+#             ((i + 1)...arr.length).each do |j|
+#                 sums << arr[i..j]
 #             end
 #         end
-#     end
-#     max_sum
+#         unsorted = sums.map {|subarr| subarr.sum}
+#         unsorted.sort[-1]
 #  end
  
+ def lcs_phase1(arr)
+    max_sum = 0
+
+    # debugger
+    arr.each_with_index do |ele1, i|
+        cumulative_sum = ele1
+        arr[(i + 1)..-1].each do |ele2|
+            cumulative_sum += ele2
+            if max_sum < cumulative_sum
+                max_sum = cumulative_sum
+            end
+        end
+    end
+    max_sum
+ end
+ 
+
 
  def lcs_phase2(arr)
-    largest_sum = 0
-    
+    largest_sum = arr.shift
+    current_sum = largest_sum
 
+    (0...arr.length).each do |i|
+        if current_sum < 0 
+            current_sum = 0
+        end
+        current_sum += arr[i]
+        
+        if largest_sum < current_sum 
+            largest_sum = current_sum 
+        end
+    end
+    largest_sum
  end
 
 
 
- list = [2, 3, -6, 7, -6, 7]
- p lcs_phase1(list)
+ list = [-5, -1, -3]
+ list2 = [5, 3, -7]
+ p lcs_phase2(list2)
